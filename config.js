1ad3ad8172088fa8e731c66877e66b073586bb54
surjitC <surjitchowdhury@gmail.com>
2018-04-10 21:19:07 +0530

    port changed 8080

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     sessionSecret: "fjeiofdalkjefiFDAEWifdhowenlw",
-    PORT: 8090,
+    PORT: 8080,
     dbUrl: "mongodb://root:root@ds119675.mlab.com:19675/healthplus",
     googleClientID: "857088890716-34jb2thvrtj7uldfv6mo0khueglon1ji.apps.googleusercontent.com",
     googleClientSecret: "ikhAkIOugP6BNOjgrmVI2ZsS",
