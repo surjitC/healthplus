eb494cf4145b5b6a1eed986d6ab477f312dcc30b
Rahul Gupta <useritch96@gmail.com>
2018-05-06 19:09:40 +0530

    modify schema

--- models/Users.js
+++ models/Users.js
@@ -2,18 +2,28 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-	googleID: String,
-	email: { type: String, unique: true },
-	password: String,
-	firstName: String,
-	lastName: String,
-	gender: String,
-	location: String,
-	address: String,
-	history: [{
-		date: Date,
-		paid: {type: Number, default: 0},
-	}]
+    googleID: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    firstName: String,
+    lastName: String,
+    gender: String,
+    location: String,
+    address: String,
+    history: [{
+        date: Date,
+        price: {
+            type: Number,
+            default: 0
+        },
+        item: {
+            type: Schema.Types.ObjectId,
+            ref: 'Products'
+        }
+    }]
 });
 
 module.exports = mongoose.model('User', userSchema);
