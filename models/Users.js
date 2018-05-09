3626331fcbaa928f6a82b12b6424464c9abeaea6
surjitC <surjitchowdhury@gmail.com>
2018-05-09 10:49:35 +0530

    pincode and contact

--- models/Users.js
+++ models/Users.js
@@ -10,8 +10,9 @@ let userSchema = new Schema({
     password: String,
     firstName: String,
     lastName: String,
+    contact: Number,
     gender: String,
-    location: String,
+    pincode: String,
     address: String,
     history: [{
         date: Date,
